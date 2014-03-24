2d4ffaa0e75a476c71af166904d78c789a22807f
Cesar Pinto Castillo <cesar@jagcesar.se>
2014-03-23 18:27:16 -0700

    Each cell is now red and is draggable sideways I override the contentView getter, that way it "just works" for the programmer who wants to use this.

--- JCRSlideCollectionViewCell/Views/JCRSlideCollectionViewCell.m
+++ JCRSlideCollectionViewCell/Views/JCRSlideCollectionViewCell.m
@@ -8,6 +8,13 @@
 
 #import "JCRSlideCollectionViewCell.h"
 
+@interface JCRSlideCollectionViewCell ()
+
+@property (nonatomic) UIScrollView *scrollView;
+@property (nonatomic) UIView *draggableContentView;
+
+@end
+
 @implementation JCRSlideCollectionViewCell
 
 - (id)initWithFrame:(CGRect)frame
@@ -15,18 +22,27 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        [self setScrollView:[UIScrollView new]];
+        [[self scrollView] setAlwaysBounceHorizontal:YES];
+        [[self scrollView] setFrame:[[super contentView] bounds]];
+        [[self scrollView] setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+        [[self scrollView] setBackgroundColor:[UIColor blueColor]];
+        [[super contentView] addSubview:[self scrollView]];
+        
+        [self setDraggableContentView:[UIView new]];
+        [[self draggableContentView] setFrame:[[self scrollView] bounds]];
+        [[self draggableContentView] setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+        [[self scrollView] addSubview:[self draggableContentView]];
+        
         [[self contentView] setBackgroundColor:[UIColor redColor]];
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+#pragma mark - Properties overrides
+
+- (UIView *)contentView {
+    return [self draggableContentView];
 }
-*/
 
 @end
