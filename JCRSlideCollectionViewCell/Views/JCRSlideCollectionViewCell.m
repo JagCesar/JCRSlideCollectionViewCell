18c550861b9ca028b376dc6a6569c23e19d2e633
Cesar Pinto Castillo <cesar@jagcesar.se>
2014-03-23 20:42:47 -0700

    Image alpha is changing when sliding The image alpha is set to 0.f when the cell is in its default place. And when the user slides it, we adjust the alpha depending on how far the cell is dragged. The alpha is 1.f when the image view is fully shown.

--- JCRSlideCollectionViewCell/Views/JCRSlideCollectionViewCell.m
+++ JCRSlideCollectionViewCell/Views/JCRSlideCollectionViewCell.m
@@ -8,7 +8,11 @@
 
 #import "JCRSlideCollectionViewCell.h"
 
-@interface JCRSlideCollectionViewCell ()
+static const CGFloat imageWidth = 22.f;
+static const CGFloat imageHeight = 22.f;
+static const CGFloat imageBorderMargin = 20.f;
+
+@interface JCRSlideCollectionViewCell () <UIScrollViewDelegate>
 
 @property (nonatomic) UIScrollView *scrollView;
 @property (nonatomic) UIView *draggableContentView;
@@ -27,21 +31,30 @@
         
         [self setLeftImageView:[UIImageView new]];
         [[self leftImageView] setContentMode:UIViewContentModeCenter];
-        [[self leftImageView] setFrame:CGRectMake(20.f, 19.f, 22.f, 22.f)];
+        [[self leftImageView] setFrame:CGRectMake(imageBorderMargin,
+                                                  CGRectGetHeight(frame)/2-imageHeight/2,
+                                                  imageWidth,
+                                                  imageHeight)];
         [[self leftImageView] setAutoresizingMask:UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleRightMargin];
         [[self leftImageView] setBackgroundColor:[UIColor purpleColor]];
+        [[self leftImageView] setAlpha:0.f];
         [[super contentView] addSubview:[self leftImageView]];
         
         [self setRightImageView:[UIImageView new]],
         [[self rightImageView] setContentMode:UIViewContentModeCenter];
-        [[self rightImageView] setFrame:CGRectMake(CGRectGetWidth(frame)-42.f, 19.f, 22.f, 22.f)];
+        [[self rightImageView] setFrame:CGRectMake(CGRectGetWidth(frame)-imageBorderMargin-imageWidth,
+                                                   CGRectGetHeight(frame)/2-imageHeight/2,
+                                                   imageWidth,
+                                                   imageHeight)];
         [[self rightImageView] setAutoresizingMask:UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleLeftMargin];
         [[self rightImageView] setBackgroundColor:[UIColor purpleColor]];
+        [[self rightImageView] setAlpha:0.f];
         [[super contentView] addSubview:[self rightImageView]];
         
         [self setScrollView:[UIScrollView new]];
         [[self scrollView] setAlwaysBounceHorizontal:YES];
         [[self scrollView] setFrame:[[super contentView] bounds]];
+        [[self scrollView] setDelegate:self];
         [[self scrollView] setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
         [[super contentView] addSubview:[self scrollView]];
         
@@ -59,6 +72,20 @@
 
 - (UIView *)contentView {
     return [self draggableContentView];
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat xOffset = [scrollView contentOffset].x;
+    if (xOffset < 0) {
+        // Draggable content view is dragged right
+        xOffset = fabsf(xOffset);
+        [[self leftImageView] setAlpha:xOffset/(imageBorderMargin+imageWidth)];
+    } else if (xOffset > 0) {
+        // Draggable content view is dragged left
+        [[self rightImageView] setAlpha:xOffset/(imageBorderMargin+imageWidth)];
+    }
 }
 
 @end
