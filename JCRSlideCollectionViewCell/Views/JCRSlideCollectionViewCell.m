3108ee26bf51f12dfdae751ff825bbd66464c16c
Cesar Pinto Castillo <cesar@jagcesar.se>
2014-03-23 18:46:08 -0700

    Created the imageViews that are behind the cell They are purple for dev purposes. The plan is for them to have an alpha of zero from start and then slowly increase as the user swipes. And when it gets to 100% of alpha to snap to the cell.
    You'll get it when we get there :P

--- JCRSlideCollectionViewCell/Views/JCRSlideCollectionViewCell.m
+++ JCRSlideCollectionViewCell/Views/JCRSlideCollectionViewCell.m
@@ -12,6 +12,8 @@
 
 @property (nonatomic) UIScrollView *scrollView;
 @property (nonatomic) UIView *draggableContentView;
+@property (nonatomic) UIImageView *leftImageView;
+@property (nonatomic) UIImageView *rightImageView;
 
 @end
 
@@ -22,11 +24,25 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        
+        [self setLeftImageView:[UIImageView new]];
+        [[self leftImageView] setContentMode:UIViewContentModeCenter];
+        [[self leftImageView] setFrame:CGRectMake(20.f, 19.f, 22.f, 22.f)];
+        [[self leftImageView] setAutoresizingMask:UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleRightMargin];
+        [[self leftImageView] setBackgroundColor:[UIColor purpleColor]];
+        [[super contentView] addSubview:[self leftImageView]];
+        
+        [self setRightImageView:[UIImageView new]],
+        [[self rightImageView] setContentMode:UIViewContentModeCenter];
+        [[self rightImageView] setFrame:CGRectMake(CGRectGetWidth(frame)-42.f, 19.f, 22.f, 22.f)];
+        [[self rightImageView] setAutoresizingMask:UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleLeftMargin];
+        [[self rightImageView] setBackgroundColor:[UIColor purpleColor]];
+        [[super contentView] addSubview:[self rightImageView]];
+        
         [self setScrollView:[UIScrollView new]];
         [[self scrollView] setAlwaysBounceHorizontal:YES];
         [[self scrollView] setFrame:[[super contentView] bounds]];
         [[self scrollView] setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
-        [[self scrollView] setBackgroundColor:[UIColor blueColor]];
         [[super contentView] addSubview:[self scrollView]];
         
         [self setDraggableContentView:[UIView new]];
