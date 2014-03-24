a190cb2f85a723e4bf95cd95aa372b9a304f7922
Cesar Pinto Castillo <cesar@jagcesar.se>
2014-03-23 17:48:29 -0700

    UICollectionView created Created an instance variable for the collectionView and for the layout

--- JCRSlideCollectionViewCell/ViewControllers/JCRViewController.m
+++ JCRSlideCollectionViewCell/ViewControllers/JCRViewController.m
@@ -8,7 +8,10 @@
 
 #import "JCRViewController.h"
 
-@interface JCRViewController ()
+@interface JCRViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (nonatomic) UICollectionView *collectionView;
+@property (nonatomic) UICollectionViewFlowLayout *collectionViewFlowLayout;
 
 @end
 
@@ -18,12 +21,39 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self setCollectionViewFlowLayout:[UICollectionViewFlowLayout new]];
+    
+    [self setCollectionView:[[UICollectionView alloc] initWithFrame:[[self view] bounds]
+                                               collectionViewLayout:[self collectionViewFlowLayout]]];
+    
+    [[self collectionView] setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+    [[self collectionView] setDataSource:self];
+    [[self collectionView] setDelegate:self];
+    [[self view] addSubview:[self collectionView]];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+#pragma mark - UICollectionViewDelegate
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
 }
 
 @end
